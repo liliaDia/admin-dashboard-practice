f08ad9b582f98dc9a66399e87e7851146a7fdd46
Morgen Diaz <morgen@webslinger.works>
2023-09-10 17:13:25 -1000

    feat: collapsable nav menu.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 const NAV_MENU = document.querySelector(".nav-menu");
+const NAV_MENU_BUTTON = document.querySelector("#hamburger");
 
 function setNavMenuOffsetTop() {
   const NAV_BAR = document.querySelector("nav");
   NAV_MENU.style.top = NAV_BAR.clientHeight + "px";
 }
 
+function openNavMenu() {
+  /* 
+        Setting display to grid when the button is clicked is a hacky way to prevent
+        the slide out animation from triggering on page load.
+    */
+  NAV_MENU.style.display = "grid";
+  NAV_MENU_BUTTON.classList.toggle("hamburger-open");
+  NAV_MENU.classList.toggle("nav-menu-open");
+}
+
 setNavMenuOffsetTop();
+NAV_MENU_BUTTON.addEventListener("click", openNavMenu);
